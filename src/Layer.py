6c65536bfde39005ba680adec4ffdede2ccc23f8
Luka Lambrecht <llambrec@cern.ch>
2022-09-16 18:45:02 +0200

    add inplace alternatives in Tensor and Layer

--- src/Layer.py
+++ src/Layer.py
@@ -10,94 +10,102 @@ from Tensor import Tensor
 from Activation import getActivation
 
 class DenseLayer():
-	### a dense layer for an ordinary neural network
+  ### a dense layer for an ordinary neural network
 	
-	weights = None
-	biasterm = None
-	input_dim = None
-	output_dim = None
-	activation = None
+  # class properties
+  weights = None
+  biasterm = None
+  input_dim = None
+  output_dim = None
+  activation = None
 	
-	def __init__(self,input_dim,output_dim,activation='linear',biasterm=True):
-		### constructor from given input and output dimension
-		# the underlying tensor is initialized randomly
-		if biasterm: 
-			initarray = np.random.rand(output_dim,input_dim+1)
-			self.biasterm = True
-		else: 
-			initarray = np.random.rand(output_dim,input_dim)
-			self.biasterm = False
-		self.weights = Tensor(initarray)
-		self.input_dim = input_dim
-		self.output_dim = output_dim
-		self.activation = getActivation(activation)
+  def __init__( self, input_dim, output_dim,
+                activation='linear', biasterm=True ):
+    ### constructor from given input and output dimension.
+    # note: the underlying tensor is initialized randomly
+    if biasterm: 
+      initarray = np.random.rand(output_dim,input_dim+1)
+      self.biasterm = True
+    else: 
+      initarray = np.random.rand(output_dim,input_dim)
+      self.biasterm = False
+    self.weights = Tensor(initarray)
+    self.input_dim = input_dim
+    self.output_dim = output_dim
+    self.activation = getActivation(activation)
 		
-	def __str__(self):
-		# string conversion overloading
-		linfo = 'DenseLayer:'
-		linfo += ' input dim = {},'.format(self.input_dim)
-		linfo += ' output dim = {},'.format(self.output_dim)
-		linfo += ' weight shape = {},'.format(self.weights.shape)
-		linfo += ' activation = {}'.format(self.activation)
-		return linfo
+  def __str__( self ):
+    ### string conversion overloading
+    linfo = 'DenseLayer:'
+    linfo += ' input dim = {},'.format(self.input_dim)
+    linfo += ' output dim = {},'.format(self.output_dim)
+    linfo += ' weight shape = {},'.format(self.weights.shape)
+    linfo += ' activation = {}'.format(self.activation)
+    return linfo
 		
-	def propagate(self,intensor,return_ext=False):
-		### calculate the layer output with current weights for a single input tensor
-		# in the default case, only the output tensor is returned, i.e. f(W.x)
-		# if return_ext is True, the derivative f'(W.x) is returned as well
-		if not (intensor.is_vector() and intensor.shape[0]==self.input_dim):
-			raise Exception('Tensor with shape {} is invalid input for DenseLayer with input dimension {}'
-									 .format(intensor.shape,self.input_dim))
-		if self.biasterm:
-			intensor = Tensor(np.append(intensor.array,np.ones((1,1))))
-		y = self.weights*intensor
-		if not return_ext:
-			return self.activation.f(y)
-		else:
-			return (self.activation.f(y), self.activation.df(y))
+  def propagate( self, intensor, 
+                 return_ext=False, inplace=False ):
+    ### calculate the layer output with current weights for a single input tensor
+    # input arguments:
+    # - return_ext: specify the returned objects.
+    #               in the default case, only the output tensor is returned, i.e. f(W.x);
+    #               if return_ext is True, the derivative f'(W.x) is returned as well
+    # - inplace: if True, the input tensor is modified in-place.
+    if not (intensor.is_vector() and intensor.shape[0]==self.input_dim):
+      msg = 'Tensor with shape {} is invalid input'.format(intensor.shape)
+      msg += ' for DenseLayer with input dimension {}'.format(self.input_dim)
+      raise Exception(msg)
+    if not inplace:
+      if self.biasterm: intensor = intensor.add_bias()
+      y = self.weights*intensor
+      if not return_ext: return self.activation.f(y)
+      else: return (self.activation.f(y), self.activation.df(y))
+    else:
+      if self.biasterm: intensor.add_bias()
+      intensor.leftmultiply_by(self.weights)
+      if not return_ext: return self.activation.f(intensor) # todo: add inplace arg
+      else: return (self.activation.f(y), self.activation.df(y)) # todo: add inplace arg
 	
-	def backpropagate(self,intensor):
-		### calculate the backpropagation tensor for this layer and given input tensor
-		# the output is a tuple:
-		# - the first element is needed when the gradient is calculated w.r.t this layer
-		# - the second element is needed for backpropagation further down 
-		df = self.propagate(intensor,return_ext=True)[1]
-		dfdiag = df.diag()
-		backweights = dfdiag*self.weights
-		if self.biasterm:
-			backweights = Tensor(backweights.array[:,:-1])
-		return (dfdiag,backweights)
+  def backpropagate( self, intensor, inplace=False ):
+    ### calculate the backpropagation tensor for this layer and given input tensor
+    # returns a tuple:
+    # - the first element is needed when the gradient is calculated w.r.t this layer
+    # - the second element is needed for backpropagation further down 
+    df = self.propagate(intensor, return_ext=True, inplace=inplace)[1]
+    df.diag(inplace=True)
+    backweights = df*self.weights
+    if self.biasterm: backweights.remove_bias(inplace=True)
+    return (df,backweights)
 	
-	def set_weights(self,weights):
-		### set the weights for this layer
-		# weights can be a Tensor or a numpy array
-		if not isinstance(weights,Tensor):
-			try:
-				weights = Tensor(weights)
-			except:
-				raise Exception('Object used to set layer weights is not a Tensor and could not be casted to one.')
-		if( self.biasterm and weights.shape!=(self.output_dim,self.input_dim+1) ):
-			raise Exception('Weight tensor has wrong shape.')
-		if( not self.biasterm and weights.shape!=(self.output_dim,self.input_dim) ):
-			raise Exception('Weight tensor has wrong shape.')
-		self.weights = weights
+  def set_weights( self, weights ):
+    ### set the weights for this layer
+    # weights can be a Tensor or a numpy array
+    if not isinstance(weights,Tensor):
+      try: weights = Tensor(weights)
+      except:
+        msg = 'Object used to set layer weights is not a Tensor'
+        msg += ' and could not be casted to one.'
+        raise Exception(msg)
+    if( self.biasterm and weights.shape!=(self.output_dim,self.input_dim+1) ):
+      raise Exception('Weight tensor has wrong shape.')
+    if( not self.biasterm and weights.shape!=(self.output_dim,self.input_dim) ):
+      raise Exception('Weight tensor has wrong shape.')
+    self.weights = weights
 		
-	def plot_weights(self,ax=None,title=''):
-		### make a plot of the current weight matrix
-		# process arguments
-		if ax is None:
-			fig,ax = plt.subplots()
-		if len(title)==0:
-			title = 'Layer weights'
-		# make the plot
-		cax = ax.matshow(self.weights.array)
-		# print the values if the dimensions are small enough
-		if( self.input_dim <= 10 and self.output_dim <= 10):
-			for (i, j), z in np.ndenumerate(self.weights.array):
-				ax.text(j, i, '{:.2f}'.format(z), ha='center', va='center')
-		ax.set_xlabel('input dimension')
-		ax.set_ylabel('output dimension')
-		plt.title(title)
-		plt.colorbar(cax)
-		plt.show()
-		return ax
+  def plot_weights(self, fig=None, ax=None, title='' ):
+    ### make a plot of the current weight matrix
+    # process arguments
+    if ax is None: fig,ax = plt.subplots()
+    if len(title)==0: title = 'Layer weights'
+    # make the plot
+    cax = ax.matshow(self.weights.array)
+    # print the values if the dimensions are small enough
+    if( self.input_dim <= 10 and self.output_dim <= 10):
+      for (i, j), z in np.ndenumerate(self.weights.array):
+        ax.text(j, i, '{:.2f}'.format(z), ha='center', va='center')
+    ax.set_xlabel('input dimension')
+    ax.set_ylabel('output dimension')
+    plt.title(title)
+    plt.colorbar(cax)
+    plt.show()
+    return (fig,ax)
--- src/Tensor.py
+++ src/Tensor.py
@@ -7,89 +7,193 @@
 import numpy as np
 
 class Tensor:
-	### class representing a 2D tensor, i.e. an array of shape (n1,n2)
+  ### class representing a 2D tensor, i.e. an array of shape (n1,n2)
+
+  # class properties
+  array = None
+  shape = None
 	
-	array = None
-	shape = None
-	
-	def __init__(self,nparray):
-		### constructor from numpy array or scalar
-		# allowed shapes are (n) or (n1,n2)
-		# in all cases, the resulting Tensor object will be of shape (n1,n2),
-		# which means input shape (n) is converted into (n,1)
-		if(len(nparray.shape)==2):
-			self.array = np.copy(nparray)
-			self.shape = self.array.shape
-		elif(len(nparray.shape)==1):
-			self.array = np.copy(np.expand_dims(nparray,axis=1))
-			self.shape = self.array.shape
-		else:
-			raise Exception('Invalid input shape for Tensor initialisation: {}'.format(nparray.shape))
+  def __init__( self, nparray ):
+    ### constructor
+    # input arguments:
+    # - nparray: numpy array for initialization.
+    #            note: allowed shapes are (n) or (n1,n2);
+    #            in all cases, the resulting Tensor object will be of shape (n1,n2),
+    #            which means input shape (n) is converted into (n,1)
+    # note: the input array gets deep copied,
+    #       so modifying the input array will not modify the Tensor.
+    # note: to check if removing the copy can give speedup.
+    if(len(nparray.shape)==2):
+      self.array = np.copy(nparray)
+      self.shape = self.array.shape
+    elif(len(nparray.shape)==1):
+      self.array = np.copy(np.expand_dims(nparray,axis=1))
+      self.shape = self.array.shape
+    else:
+      raise Exception('Invalid input shape for Tensor initialisation: {}'.format(nparray.shape))
 			
-	def __str__(self):
-		### string conversion overloading
-		return str(self.array)
+  def __str__( self ):
+    ### string conversion overloading
+    return str(self.array)
 			
-	def __getitem__(self,indices):
-		### element access overloading
-		# note that python implicitly converts multidim indices to a tuple
-		# no index out of bounds checking since implicit in numpy array
-		return self.array[indices[0],indices[1]]
+  def __getitem__( self, indices ):
+    ### element access overloading
+    # note: python implicitly converts multidim indices to a tuple,
+    #       so no index out of bounds checking needed here,
+    #       since implicit in numpy array
+    return self.array[indices[0],indices[1]]
+
+  def add_bias( self, bias=1, inplace=False ):
+    ### add a bias term to a vector tensor
+    if not self.is_vector():
+      raise Exception('Cannot add a bias term to a tensor of shape {}'.format(self.shape))
+    if not inplace: return Tensor(np.append(self.array,np.ones((1,1))*bias))
+    else: self.array = np.append(self.array,np.ones((1,1))*bias)
+
+  def remove_bias( self, inplace=False ):
+    ### remove a bias terms (assumed to be in the last colunn of a 2D tensor)
+    if not inplace: return Tensor(self.array[:,:-1])
+    else: self.array = self.array[:,:-1]
 		
-	def __add__(self,other):
-		### addition overloading
-		if( self.is_scalar() ):
-			return Tensor( self[0,0]+other.array )
-		elif( other.is_scalar() ):
-			return Tensor( self.array+other[0,0] )
-		elif(self.shape!=other.shape):
-			raise Exception('Tensors with shapes {} and {} cannot be added.'.format(self.shape,other.shape))
-		return Tensor(self.array + other.array)
-	
-	def __sub__(self,other):
-		### subtraction overloading
-		return self + Tensor(-other.array)
+  def __add__( self, other ):
+    ### addition overloading
+    # note: a new tensor is created that is the sum of both,
+    #       leaving the original two tensors unmodified.
+    if( self.is_scalar() ):
+      return Tensor( self[0,0]+other.array )
+    elif( other.is_scalar() ):
+      return Tensor( self.array+other[0,0] )
+    elif(self.shape!=other.shape):
+      raise Exception('Tensors with shapes {} and {} cannot be added.'.format(self.shape,other.shape))
+    return Tensor(self.array + other.array)
+
+  def increase_by( self, other ):
+    ### add another tensor to the current one
+    # note: the original tensor is modified in-place.
+    if( self.is_scalar() ): 
+      self.array = self[0,0] + other.array
+      self.shape = self.array.shape
+    elif( other.is_scalar() ):
+      self.array = self.array + other[0,0]
+    elif(self.shape!=other.shape):
+      raise Exception('Tensors with shapes {} and {} cannot be added.'.format(self.shape,other.shape))
+    self.array += other.array
+
+  def __neg__( self ):
+    ### unary minus sign overloading
+    # note: a new tensor is created that is the negative of the original one,
+    #       leaving the original tensor unmodified.
+    return Tensor(-self.array)
+
+  def negate( self ):
+    ### negate the current tensor
+    # note: the original tensor is modified in-place.
+    self.array = -self.array
+
+  def __sub__( self, other ):
+    ### subtraction overloading
+    # note: a new tensor is created that is the subtraction of both,
+    #       leaving the original two tensors unmodified.
+    return self + (-other)
+
+  def decrease_by( self, other ):
+    ### subtract another tensor from the current one
+    # note: the original tensor is modified in-place.
+    if( self.is_scalar() ):
+      self.array = self[0,0] - other.array
+      self.shape = self.array.shape
+    elif( other.is_scalar() ):
+      self.array = self.array - other[0,0]
+    elif(self.shape!=other.shape):
+      raise Exception('Tensors with shapes {} and {} cannot be subtracted.'.format(self.shape,other.shape))
+    self.array -= other.array
 				
-	def __mul__(self,other):
-		### multiplication overloading
-		# the default behaviour is matrix multiplication, i.e. (n1,m)*(m,n2) -> (n1,n2)
-		# if one of the tensors is scalar, element-wise multiplication is performed.
-		if( self.is_scalar() ):
-			return Tensor( self[0,0]*other.array )
-		elif( other.is_scalar() ):
-			return Tensor( self.array*other[0,0] )
-		elif(self.shape[1]!=other.shape[0]):
-			raise Exception('Tensors with shapes {} and {} cannot be multiplied.'.format(self.shape,other.shape))
-		return Tensor( np.matmul(self.array,other.array) )
+  def __mul__( self, other ):
+    ### multiplication overloading
+    # note: the default behaviour is matrix multiplication, i.e. (n1,m)*(m,n2) -> (n1,n2)
+    #       if one of the tensors is scalar, element-wise multiplication is performed.
+    # note: a new tensor is created that is the multiplication of both,
+    #       leaving the original two tensors unmodified.
+    if( self.is_scalar() ):
+      return Tensor( self[0,0]*other.array )
+    elif( other.is_scalar() ):
+      return Tensor( self.array*other[0,0] )
+    elif(self.shape[1]!=other.shape[0]):
+      raise Exception('Tensors with shapes {} and {} cannot be multiplied.'.format(self.shape,other.shape))
+    return Tensor( np.matmul(self.array,other.array) )
+
+  def rightmultiply_by( self, other ):
+    ### multiply this tensor with another one.
+    # schematically: this = this * other.
+    # note: the default behaviour is matrix multiplication, i.e. (n1,m)*(m,n2) -> (n1,n2)
+    #       if one of the tensors is scalar, element-wise multiplication is performed.
+    # note: the original tensor is modified in-place.
+    if( self.is_scalar() ):
+      self.array = self[0,0]*other.array
+      self.shape = self.array.shape
+    elif( other.is_scalar() ):
+      self.array *= other[0,0]
+    elif(self.shape[1]!=other.shape[0]):
+      raise Exception('Tensors with shapes {} and {} cannot be multiplied.'.format(self.shape,other.shape))
+    self.array = np.matmul(self.array, other.array)
+    self.shape = self.array.shape
+
+  def leftmultiply_by( self, other ):
+    ### multiply this tensor with another one.
+    # schematically: this = other * this.
+    # note: the default behaviour is matrix multiplication, i.e. (n1,m)*(m,n2) -> (n1,n2)
+    #       if one of the tensors is scalar, element-wise multiplication is performed.
+    # note: the original tensor is modified in-place.
+    if( self.is_scalar() ):
+      self.array = self[0,0]*other.array
+      self.shape = self.array.shape
+    elif( other.is_scalar() ):
+      self.array *= other[0,0]
+    elif(other.shape[1]!=self.shape[0]):
+      raise Exception('Tensors with shapes {} and {} cannot be multiplied.'.format(other.shape,self.shape))
+    self.array = np.matmul(other.array, self.array)
+    self.shape = self.array.shape
 		
-	def transpose(self):
-		### transpose a tensor
-		# the input and output by definition always have a 2D shape
-		return Tensor(np.transpose(self.array))
+  def transpose( self, inplace=False ):
+    ### transpose a tensor
+    # the input and output by definition always have a 2D shape
+    if not inplace: return Tensor(np.transpose(self.array))
+    else: self.array = np.transpose(self.array)
 	
-	def is_vector(self):
-		### check whether tensor has shape (n,1)
-		return self.shape[1]==1
+  def is_vector( self ):
+    ### check whether tensor has shape (n,1)
+    return self.shape[1]==1
 	
-	def is_scalar(self):
-		### check whether tensor has shape (1,1)
-		return self.shape==(1,1)
+  def is_scalar( self ):
+    ### check whether tensor has shape (1,1)
+    return self.shape==(1,1)
 	
-	def diag(self):
-		### make a diagonal matrix out of a vector
-		if not self.is_vector:
-			raise Exception('Cannot make a diagonal Tensor out of a Tensor with shape {}'.format(self.shape))
-		return Tensor(np.diag(np.squeeze(self.array,axis=1)))
+  def diag( self, inplace=False ):
+    ### make a diagonal matrix out of a vector
+    if not self.is_vector():
+      raise Exception('Cannot make a diagonal Tensor out of a Tensor with shape {}'.format(self.shape))
+    if not inplace: return Tensor(np.diag(np.squeeze(self.array,axis=1)))
+    else:
+      self.array = np.diag(np.squeeze(self.array,axis=1))
+      self.shape = self.array.shape
 	
-	def contains_values_above(self,threshold):
-		### return whether a tensor contains values (absolute value) above threshold
-		arr = np.abs(self.array)
-		return np.any(arr>threshold)
+  def contains_values_above(self,threshold):
+    ### return whether a tensor contains values (absolute value) above threshold
+    arr = np.abs(self.array)
+    return np.any(arr>threshold)
 	
-	def squared(self):
-		### element-wise square
-		return Tensor( np.square(self.array) )
+  def squared( self, inplace=False ):
+    ### element-wise square
+    if not inplace: return Tensor( np.square(self.array) )
+    else: self.array = np.square(self.array)
 	
-	def divide(self,other):
-		### element-wise division
-		return Tensor( np.divide(self.array,other.array) )
+  def divide( self, other ):
+    ### element-wise division
+    # note: a new tensor is created that is the multiplication of both,
+    #       leaving the original two tensors unmodified.
+    return Tensor( np.divide(self.array,other.array) )
+
+  def divide_by( self, other ):
+    ### divide the elements of the current tensor by another one
+    # note: the original tensor is modified in-place.
+    self.array = np.divide(self.array,other.array)
